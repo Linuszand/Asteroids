d9d39e87174743aac7021932b66123caee1ba773
Linuszand <linus.zand@gmail.com>
2024-10-30 22:16:57 +0100

    Added white triangle in center of ze screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -7,13 +8,21 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    new_triangle = Player(x, y)
+    derp = 0
+    time = pygame.time.Clock()
     while True:
-        screen.fill("black")
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+
+        screen.fill("black")
+        new_triangle.draw(screen)
+        pygame.display.flip()
+
+        derp = time.tick(60)/1000
 
 if __name__ == "__main__":
     main()
