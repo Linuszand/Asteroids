04e152354fea43089f2c550196ad81ead378acac
Linuszand <linus.zand@gmail.com>
2024-11-01 23:58:52 +0100

    The game is complete, but can keep adding game stuff.

--- main.py
+++ main.py
@@ -1,28 +1,65 @@
 import pygame
 from constants import *
 from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
+from circleshape import CircleShape
+from shot import Shot
 
 def main():
     pygame.init()
+    pygame.font.init()
+    the_font = pygame.font.SysFont('Comic Sans MS', 30)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
-    new_triangle = Player(x, y)
     derp = 0
     time = pygame.time.Clock()
-    while True:
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    asteroidfield = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    new_triangle = Player(x, y)
+    asteroid_field = AsteroidField()
+    score = 0
+  
+
+    while running:
+        text_surf = the_font.render(str(score), False, (255, 255, 255))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        for obj in updatable:
+            obj.update(derp)
+
+        for asteroid in asteroids:
+            if new_triangle.collision(asteroid):
+                print("Game over!")
+                running = False
+            for shot in shots:
+                if asteroid.collision(shot):
+                    asteroid.split()
+                    shot.kill()
+                    score += 1
         screen.fill("black")
-        new_triangle.draw(screen)
+        screen.blit(text_surf, (0, 0))
+        for obj in drawable:
+            obj.draw(screen)
+        
+
         pygame.display.flip()
 
         derp = time.tick(60)/1000
-
+        
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,13 +1,17 @@
 from circleshape import CircleShape
 from constants import *
+from shot import Shot
 import pygame
+import math
 
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-        
+        self.x = x
+        self.y = y
+        self.timer = 0
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -19,3 +23,38 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
+    def rotate(self, dt):
+        self.rotation = self.rotation + PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        self.timer -= dt
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt - (dt * 2))
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        if keys[pygame.K_SPACE] and self.timer <= 0:
+            self.shoot()
+            self.timer = PLAYER_SHOOT_COOLDOWN
+                
+                
+                
+
+    def move(self, dt):
+        angle_in_radians = self.rotation * (math.pi / 180)
+        x = -math.sin(angle_in_radians)
+        y = math.cos(angle_in_radians)
+    
+        # Update position's x and y components directly - Updates the value of self.position
+        self.position[0] += x * PLAYER_SPEED * dt  # x component
+        self.position[1] += y * PLAYER_SPEED * dt  # y component
+        #self.position += movement * PLAYER_SPEED * dt
+   
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
